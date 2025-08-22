ca52fb6527375a065baaa52fa234b2272802fba8
Fabiano2503 <fabianoaimar@gmail.com>
2025-08-22 11:55:29 -0500

    añadir ruta '/actividad' para obtener actividades desde API

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app.config['SECRET_KEY'] = '12345678'
 def index():
     return render_template('index.html')
 
+@app.route('/actividad', methods=['GET'])
+def actividad():
+    # URL DE LA API: 'http://bored.api.lewagon.com/api/activity/'
+    return "Ruta Actividad"
+
 if __name__ == '__main__':
     app.run(debug=True)
