4f028e9939e65fadef9706f5059ea7d53e1125aa
Fabiano2503 <fabianoaimar@gmail.com>
2025-08-22 11:35:35 -0500

    crear ruta principal que renderiza el template 'index.html'

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = '12345678'
 
 @app.route('/')
 def index():
-    return "Ruta Principal"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
