7b680509a7ad5f3fffbe24e88c9876b730b04ab2
Fabiano2503 <fabianoaimar@gmail.com>
2025-08-22 12:28:17 -0500

    agregar lógica para manejar parámetro 'type' en la URL

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -10,8 +11,30 @@ def index():
 
 @app.route('/actividad', methods=['GET'])
 def actividad():
-    # URL DE LA API: 'http://bored.api.lewagon.com/api/activity/'
-    return "Ruta Actividad"
+    # OBTENER PARAMETRO 'TYPE' DE LA URL
+    actividad_type = request.args.get('type')
+
+    # URL DE LA API
+    url_api = 'http://bored.api.lewagon.com/api/activity/'
+
+    if actividad_type:
+        # SI HAY TIPO DE ACTIVIDAD AGREGARLO A LA URL
+        url = f'{url_api}?type={actividad_type}'
+    else:
+        # SI NO HAY GENERAR ACTIVIDAD ALEATORIA
+        url = url_api
+
+    # SOLICITUD A LA API
+    response = requests.get(url)
+
+    # OBTENER LOS DATOS EN JSON
+    actividad_data = response.json()
+
+    # FILTRAR SOLO LA ACTIVIDAD Y SI NO SE ENCUENTRA SE MANDA UN MENSAJE
+    actividad = actividad_data.get('activity', 'No se encontro actividad')
+
+    # MOSTRAR ACTIVIDAD
+    return actividad
 
 if __name__ == '__main__':
     app.run(debug=True)
